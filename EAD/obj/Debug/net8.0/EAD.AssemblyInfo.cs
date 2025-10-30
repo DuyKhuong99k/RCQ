fd1838938d57bee3eea02b12edfd8e2aeaee27a1
nhphuc699 <nhphuc699@gmail.com>
2025-10-30 14:58:21 +0700

    Update assembly informational versions and package dependencies

--- EAD/obj/Debug/net8.0/EAD.AssemblyInfo.cs
+++ EAD/obj/Debug/net8.0/EAD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e21b38e9eefbf3bfe9d8d4a54b33627f86f52b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079f0af1bd60b307be9d0e6bb93dbb228285ad21")]
 [assembly: System.Reflection.AssemblyProductAttribute("EAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PMSHub/obj/Debug/net8.0/PMSHub.AssemblyInfo.cs
+++ PMSHub/obj/Debug/net8.0/PMSHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMSHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e21b38e9eefbf3bfe9d8d4a54b33627f86f52b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079f0af1bd60b307be9d0e6bb93dbb228285ad21")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMSHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMSHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
