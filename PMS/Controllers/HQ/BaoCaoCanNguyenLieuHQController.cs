97d2467acb60e4efff4d4342887bae46562abcad
hataba123 <nguyenloi.coder@gmail.com>
2026-05-26 15:32:31 +0700

    fix conflict merge

--- PMS/Controllers/HQ/BaoCaoCanNguyenLieuHQController.cs
+++ PMS/Controllers/HQ/BaoCaoCanNguyenLieuHQController.cs
@@ -6,12 +6,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using Models.Repos;
-using Models.Repos.Models;
 using Newtonsoft.Json;
 using PMS.Attrs;
 using PMS.Models;
@@ -56,6 +50,155 @@ namespace PMS.Controllers.HQ
             }
             ViewBag.TitlePage = "Báo Cáo Chi Tiết NL Nhập";
             return View("~/Views/BaoCaoNguyenLieuHQ/ChiTietNLNHQView.cshtml");
+        }
+        [HttpPost]
+        public IActionResult UpdateKhoiLuong(
+    string soPhieuCanNhap,
+    string maQuyCach,
+    decimal khoiLuongNhap)
+        {
+            try
+            {
+                var query = @"
+IF EXISTS (
+    SELECT 1
+    FROM HQ_ChiTietPhanBoTyLeNguyenLieuNhap
+    WHERE SoPhieuCanNhap = @SoPhieuCanNhap
+)
+BEGIN
+
+    -- Có dữ liệu thì update
+			DECLARE @TongTrongLuong FLOAT
+			DECLARE @KhoiLuongCu FLOAT
+			DECLARE @ChenhLech FLOAT
+			DECLARE @MaQuyCachBu VARCHAR(50)
+
+			-- Tổng phiếu
+			SELECT @TongTrongLuong = TrongLuongHang
+			FROM HQ_PhieuCanNhapNguyenLieu
+			WHERE SoPhieuCanNhap = @SoPhieuCanNhap
+
+			-- Khối lượng cũ của dòng đang sửa
+			SELECT @KhoiLuongCu =
+				(TyLe * @TongTrongLuong) / 100.0
+			FROM HQ_ChiTietPhanBoTyLeNguyenLieuNhap
+			WHERE SoPhieuCanNhap = @SoPhieuCanNhap
+			AND MaQuyCach = @MaQuyCach
+
+			-- Chênh lệch
+			SET @ChenhLech = @KhoiLuongNhap - @KhoiLuongCu
+
+			------------------------------------------------
+			-- Update dòng đang sửa
+			------------------------------------------------
+			UPDATE HQ_ChiTietPhanBoTyLeNguyenLieuNhap
+			SET TyLe =
+				(@KhoiLuongNhap * 100.0)
+				/ NULLIF(@TongTrongLuong,0)
+			WHERE SoPhieuCanNhap = @SoPhieuCanNhap
+			AND MaQuyCach = @MaQuyCach
+
+			------------------------------------------------
+			-- Lấy dòng đầu tiên khác để bù
+			------------------------------------------------
+			SELECT TOP 1
+				@MaQuyCachBu = MaQuyCach
+			FROM HQ_ChiTietPhanBoTyLeNguyenLieuNhap
+			WHERE SoPhieuCanNhap = @SoPhieuCanNhap
+			AND MaQuyCach <> @MaQuyCach
+
+			------------------------------------------------
+			-- Update dòng bù
+			------------------------------------------------
+			UPDATE pn
+			SET pn.TyLe =
+			(
+				(
+					((pn.TyLe * @TongTrongLuong) / 100.0)
+					- @ChenhLech
+				) * 100.0
+			) / NULLIF(@TongTrongLuong,0)
+
+			FROM HQ_ChiTietPhanBoTyLeNguyenLieuNhap pn
+			WHERE pn.SoPhieuCanNhap = @SoPhieuCanNhap
+			AND pn.MaQuyCach = @MaQuyCachBu
+
+END
+ELSE
+BEGIN
+
+    -- Không có dữ liệu thì chạy câu khác
+			 DECLARE @TongTrongLuong FLOAT
+			DECLARE @KhoiLuongCu FLOAT
+			DECLARE @ChenhLech FLOAT
+			DECLARE @MaQuyCachBu VARCHAR(50)
+
+			------------------------------------------------
+			-- Tổng trọng lượng phiếu
+			------------------------------------------------
+			SELECT @TongTrongLuong = TrongLuongHang
+			FROM HQ_PhieuCanNhapNguyenLieu
+			WHERE SoPhieuCanNhap = @SoPhieuCanNhap
+
+			------------------------------------------------
+			-- Lấy trọng lượng cũ của dòng đang sửa
+			------------------------------------------------
+			SELECT @KhoiLuongCu = TrongLuongBaoLua
+			FROM HQ_ChiTietPhanBoBaoLuaNguyenLieuNhap
+			WHERE SoPhieuCanNhap = @SoPhieuCanNhap
+			AND MaQuyCach = @MaQuyCach
+
+			------------------------------------------------
+			-- Chênh lệch
+			------------------------------------------------
+			SET @ChenhLech = @KhoiLuongNhap - @KhoiLuongCu
+
+			------------------------------------------------
+			-- Update dòng đang sửa
+			------------------------------------------------
+			UPDATE bl
+			SET bl.TrongLuongBaoLua = @KhoiLuongNhap
+			FROM HQ_ChiTietPhanBoBaoLuaNguyenLieuNhap bl
+			WHERE bl.SoPhieuCanNhap = @SoPhieuCanNhap
+			AND bl.MaQuyCach = @MaQuyCach
+
+			------------------------------------------------
+			-- Lấy dòng đầu tiên khác để bù
+			------------------------------------------------
+			SELECT TOP 1
+				@MaQuyCachBu = MaQuyCach
+			FROM HQ_ChiTietPhanBoBaoLuaNguyenLieuNhap
+			WHERE SoPhieuCanNhap = @SoPhieuCanNhap
+			AND MaQuyCach <> @MaQuyCach
+
+			------------------------------------------------
+			-- Update dòng bù để cân bằng tổng
+			------------------------------------------------
+			UPDATE bl
+			SET bl.TrongLuongBaoLua =
+				bl.TrongLuongBaoLua - @ChenhLech
+			FROM HQ_ChiTietPhanBoBaoLuaNguyenLieuNhap bl
+			WHERE bl.SoPhieuCanNhap = @SoPhieuCanNhap
+			AND bl.MaQuyCach = @MaQuyCachBu
+
+END";
+
+                using var connection = new SqlConnection(AppViewModels.Base.Ins.ConnectionString);
+                connection.Open();
+
+                var rows = connection.Execute(query, new
+                {
+                    soPhieuCanNhap,
+                    maQuyCach,
+                    khoiLuongNhap
+                });
+
+                return Json(new { success = rows > 0 });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
         }
         public async Task<IEnumerable<object>> GetChiTietPhieuCanNhapNguyenLieus(DateTime? fromDate = null, DateTime? toDate = null, string xuongId = null)
         {
